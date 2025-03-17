fef6ecebbd530634c2394ac6af6f443b1cd622ce
Evan Hemsley <2342303+thatcosmonaut@users.noreply.github.com>
2025-03-17 13:19:41 -0700

    GPU: Fix Vulkan backend never checking deallocations (#12567)
    ---------
    
    Co-authored-by: Sam Lantinga <slouken@libsdl.org>
    (cherry picked from commit 0bd70684b35ac6cae114e25a2fdca9f430e0b3b1)

--- src/gpu/vulkan/SDL_gpu_vulkan.c
+++ src/gpu/vulkan/SDL_gpu_vulkan.c
@@ -1135,6 +1135,7 @@ struct VulkanRenderer
 
     VulkanMemoryAllocator *memoryAllocator;
     VkPhysicalDeviceMemoryProperties memoryProperties;
+    bool checkEmptyAllocations;
 
     WindowData **claimedWindows;
     Uint32 claimedWindowCount;
@@ -1576,6 +1577,10 @@ static void VULKAN_INTERNAL_RemoveMemoryUsedRegion(
         usedRegion->allocation,
         usedRegion->offset,
         usedRegion->size);
+
+    if (usedRegion->allocation->usedRegionCount == 0) {
+        renderer->checkEmptyAllocations = true;
+    }
 
     SDL_free(usedRegion);
 
@@ -10430,7 +10435,6 @@ static bool VULKAN_Submit(
     VkPipelineStageFlags waitStages[MAX_PRESENT_COUNT];
     Uint32 swapchainImageIndex;
     VulkanTextureSubresource *swapchainTextureSubresource;
-    Uint8 commandBufferCleaned = 0;
     VulkanMemorySubAllocator *allocator;
     bool presenting = false;
 
@@ -10546,12 +10550,10 @@ static bool VULKAN_Submit(
                 renderer,
                 renderer->submittedCommandBuffers[i],
                 false);
-
-            commandBufferCleaned = 1;
         }
     }
 
-    if (commandBufferCleaned) {
+    if (renderer->checkEmptyAllocations) {
         SDL_LockMutex(renderer->allocatorLock);
 
         for (Uint32 i = 0; i < VK_MAX_MEMORY_TYPES; i += 1) {
@@ -10566,6 +10568,8 @@ static bool VULKAN_Submit(
                 }
             }
         }
+
+        renderer->checkEmptyAllocations = false;
 
         SDL_UnlockMutex(renderer->allocatorLock);
     }
