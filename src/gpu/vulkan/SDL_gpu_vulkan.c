3e3d4acdc1fd34195191c4b3c49e12d85d9c2d94
Evan Hemsley <2342303+thatcosmonaut@users.noreply.github.com>
2025-04-04 14:27:18 -0700

    GPU Vulkan: Clean up in Submit in headless mode (#12744)
    (cherry picked from commit 33f90f2e41a1567b2d40c00e8e8869e9573ca4e5)

--- src/gpu/vulkan/SDL_gpu_vulkan.c
+++ src/gpu/vulkan/SDL_gpu_vulkan.c
@@ -10458,7 +10458,9 @@ static bool VULKAN_Submit(
     Uint32 swapchainImageIndex;
     VulkanTextureSubresource *swapchainTextureSubresource;
     VulkanMemorySubAllocator *allocator;
-    bool presenting = (vulkanCommandBuffer->presentDataCount > 0);
+    bool performCleanups =
+        (renderer->claimedWindowCount > 0 && vulkanCommandBuffer->presentDataCount > 0) ||
+        renderer->claimedWindowCount == 0;
 
     SDL_LockMutex(renderer->submitLock);
 
@@ -10481,7 +10483,7 @@ static bool VULKAN_Submit(
             swapchainTextureSubresource);
     }
 
-    if (presenting &&
+    if (performCleanups &&
         renderer->allocationsToDefragCount > 0 &&
         !renderer->defragInProgress) {
         if (!VULKAN_INTERNAL_DefragmentMemory(renderer, vulkanCommandBuffer))
@@ -10565,8 +10567,7 @@ static bool VULKAN_Submit(
             (presentData->windowData->frameCounter + 1) % renderer->allowedFramesInFlight;
     }
 
-    // If presenting, check if we can perform any cleanups
-    if (presenting) {
+    if (performCleanups) {
         for (Sint32 i = renderer->submittedCommandBufferCount - 1; i >= 0; i -= 1) {
             vulkanResult = renderer->vkGetFenceStatus(
                 renderer->logicalDevice,
