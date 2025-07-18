4d2d19ae6f5d296a1a5467733814298ce7729be3
Acclution <42910256+Acclution@users.noreply.github.com>
2025-07-18 22:27:31 +0000

    GPU: Fix Vulkan compute uniform descriptor not being marked as set (#13389)
    (cherry picked from commit 8bd29f7ca3612af10fc2ca54bce6b5ca01091e97)

--- src/gpu/vulkan/SDL_gpu_vulkan.c
+++ src/gpu/vulkan/SDL_gpu_vulkan.c
@@ -8591,7 +8591,7 @@ static void VULKAN_INTERNAL_BindComputeDescriptorSets(
         dynamicOffsetCount,
         dynamicOffsets);
 
-    commandBuffer->needNewVertexUniformOffsets = false;
+    commandBuffer->needNewComputeUniformOffsets = false;
 }
 
 static void VULKAN_DispatchCompute(
